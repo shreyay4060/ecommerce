a090a4d2a2f5028126a537b0e0f024c7a6140f81
shreyay4060 <shreyay4060@gmail.com>
2025-06-17 02:04:32 +0530

    update viteConfig.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/', // 👈 ensures correct base path for Vercel
+  plugins: [react(), tailwindcss()],
 })
